7eade1410c8d894bf3f984b3f54c7b289961a13d
drmohotti <drmohotti@gmail.com>
2020-04-16 22:24:25 +0530

    IT18366082 crd

--- health_care_system_paf/src/com/visits.java
+++ health_care_system_paf/src/com/visits.java
@@ -1,5 +1,191 @@
 package com;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+
+import com.mysql.jdbc.PreparedStatement;
+import com.mysql.jdbc.Statement;
 
 public class visits {
+
+	Connection con = null;            
+
+
+public Connection connect() 
+{  
+	Connection con = null;            
 	
+	
+	try        
+	{         
+		Class.forName("com.mysql.jdbc.Driver");         
+		con= DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/healthcare","root", "");    
+
+		System.out.print("\nSuccessfully connected");  
+   }        
+   catch(Exception e)   
+   {        
+		System.out.print("\nNot connected");  
+
+	   e.printStackTrace();        
+   }              
+   return con;
+}
+
+public String insertVisits(String hospital_name, String hospital_city, String date, String time,String noPatients) {
+
+	String output = ""; 
+	 
+	 try  
+	 {
+		 Connection con = connect(); 
+		 if (con == null) 
+		 { 
+			 return "\nError while connecting to the database";  
+		 } 
+	 
+	  // create a prepared statement  
+	String query = "insert into doctor_visiting values(?, ?, ?, ?, ?,?)"; 
+	 
+	PreparedStatement preparedStmt = (PreparedStatement) con.prepareStatement(query); 
+	 
+	  // binding values  
+	  preparedStmt.setInt(1, 0);  
+	  preparedStmt.setString(2, hospital_name);  
+	  preparedStmt.setString(3, hospital_city); 
+	  //preparedStmt.setDouble(4, Double.parseDouble(price)); 
+	  preparedStmt.setString(4, date); 
+	  preparedStmt.setString(5, time); 
+	  preparedStmt.setString(6, noPatients); 
+
+	//execute the statement
+	  preparedStmt.execute();
+	  con.close(); 
+	  
+	  output = "\nInserted successfully";  
+		 System.out.print("inserted");  
+
+	} 
+	catch(Exception e) 
+	{  
+		 output= "\nError while inserting"; 
+		 System.err.println(e.getMessage()); 
+		 System.out.println("not inserted");  
+
+	} 
+	 
+	 return output; 
+	 
+	
+}
+
+public String readVisits()
+{
+	String output = "";  
+	
+	try
+	{  
+		Connection con = connect(); 
+		if (con == null) 
+		{  
+			return "Error while connecting to the database for reading.";  
+		}
+		
+		output="<table border=\"1\"><tr><th>Hospital Name</th>"
+				+ "<th>Hospital City</th>"
+				+ "<th>Date</th><th>Time</th>"
+				+ "<th>No. of patients</th>"
+				+ "<th>Update</th><th>Remove</th>"
+				+ "</tr>";
+		
+
+		String query = "select * from doctor_visiting"; 
+		Statement stmt = (Statement) con.createStatement();  
+		ResultSet rs = stmt.executeQuery(query); 
+		
+		// iterate through the rows in the result set 
+		while (rs.next())
+		{   
+			String visiting_id = Integer.toString(rs.getInt("visiting_id")); 
+			String hospitalName = rs.getString("hospital_name");
+			String city = rs.getString("hospital_city"); 
+			String date = rs.getString("date");    
+			String time = rs.getString("time"); 
+			String noPatients = rs.getString("noPatients"); 
+
+		
+		
+
+	    // Add into the html table  
+		output += "<tr><td>" + hospitalName + "</td>";	
+		output += "<td>" + city + "</td>"; 	  
+		output += "<td>" + date + "</td>";  	  
+		output += "<td>" + time + "</td>"; 
+		output += "<td>" + noPatients + "</td>"; 
+		
+		
+		// buttons    
+		output += "<td><input name=\"btnUpdate\" "     
+		+ " type=\"button\" value=\"Update\"></td>"     
+		+ "<td><form method=\"post\" action=\"visiting.jsp\">"     
+		+ "<input name=\"btnRemove\" "     
+		+ " type=\"submit\" value=\"Remove\">"     
+		+ "<input name=\"visiting_id\" type=\"hidden\" "    
+		+ " value=\"" + visiting_id + "\">" 
+		+ "</form></td></tr>";   } 
+		 
+		con.close(); 
+		 
+	// Complete the html table  
+		output += "</table>"; 
+		 
+		
+
+	} 
+	catch(Exception e) 
+	{  
+		output = "Error while reading the items.";
+		System.err.println(e.getMessage()); 
+	}
+	return output; 
+}
+
+public String deleteVisits(String visiting_id) 
+{ 
+	String output = ""; 
+	
+	try
+	{ 
+		Connection con = connect();
+		if (con == null)
+		{  
+			return "Error while connecting to the database for deleting."; 
+		} 
+
+		// create a prepared statement 
+		String query = "delete from doctor_visiting where visiting_id=?"; 
+		 
+		PreparedStatement preparedStmt =(PreparedStatement) con.prepareStatement(query);
+		 
+		// binding values
+		preparedStmt.setInt(1, Integer.parseInt(visiting_id));    
+		// execute the statement preparedStmt.execute(); con.close(); 
+		 
+		// execute the statement   
+		preparedStmt.execute(); 
+		con.close(); 
+		
+		output = "Deleted successfully"; 
+	}
+	catch (Exception e) 
+	{  
+		output = "Error while deleting the item.";  
+		System.err.println(e.getMessage()); 
+	}
+	return output;
+} 
+
+
+
+
 }
