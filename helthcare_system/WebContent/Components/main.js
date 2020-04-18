3af077c2d697d16dadbb8638c57c1c0ea374a598
drmohotti <drmohotti@gmail.com>
2020-04-18 11:53:56 +0530

    client side + server + crud 4.2

--- helthcare_system/WebContent/Components/main.js
+++ helthcare_system/WebContent/Components/main.js
@@ -1,3 +1,4 @@
+
 $(document).ready(function() {
 	if ($("#alertSuccess").text().trim() == "")  
 	{  
@@ -27,15 +28,15 @@ $(document).on("click","#btnSave",function(event)
 
 			// If valid----------------------
 			$("#formVisit").submit();
-		/*	var student = getStudentCard($("#txtName").val().trim(), $(
-					'input[name="rdoGender"]:checked').val(), $("#ddlYear")
-					.val());
-			$("#colStudents").append(student);
-			$("#alertSuccess").text("Saved successfully.");
-			$("#alertSuccess").show();
-			$("#formStudent")[0].reset();*/
+		/*
+		 * var student = getStudentCard($("#txtName").val().trim(), $(
+		 * 'input[name="rdoGender"]:checked').val(), $("#ddlYear") .val());
+		 * $("#colStudents").append(student); $("#alertSuccess").text("Saved
+		 * successfully."); $("#alertSuccess").show();
+		 * $("#formStudent")[0].reset();
+		 */
 		});
-//UPDATE==========================================
+// UPDATE==========================================
 $(document).on("click",".btnUpdate",function(event) 
 		{
 			$("#hidVisitIDSave").val($(this).closest("tr").find('#hidVisitIDUpdate').val());
@@ -56,19 +57,12 @@ $(document).on("click", ".remove", function(event) {
 // CLIENT-MODEL=================================================================
 function validateVisitForm() {
 	// NAME
-	/*if ($("#txtName").val().trim() == "") {
-		return "Insert student name.";
-	}
-
-	// GENDER
-	if ($('input[name="rdoGender"]:checked').length === 0) {
-		return "Select gender.";
-	}
-
-	// YEAR
-	if ($("#ddlYear").val() == "0") {
-		return "Select year.";
-	}*/
+	/*
+	 * if ($("#txtName").val().trim() == "") { return "Insert student name."; } //
+	 * GENDER if ($('input[name="rdoGender"]:checked').length === 0) { return
+	 * "Select gender."; } // YEAR if ($("#ddlYear").val() == "0") { return
+	 * "Select year."; }
+	 */
 if ($("#hospital_name").val().trim() == "") {
 		return "Insert Hospital Name.";
 	}
@@ -89,34 +83,115 @@ if ($("#hospital_name").val().trim() == "") {
 	return true;
 }
 
-/*function getStudentCard(name, gender, year) {
-	var title = (gender == "Male") ? "Mr." : "Ms.";
-	var yearNumber = "";
+/*
+ * function getStudentCard(name, gender, year) { var title = (gender == "Male") ?
+ * "Mr." : "Ms."; var yearNumber = "";
+ * 
+ * switch (year) { case "1": yearNumber = "1st"; break; case "2": yearNumber =
+ * "2nd"; break; case "3": yearNumber = "3rd"; break; case "4": yearNumber =
+ * "4th"; break; }
+ * 
+ * var student = ""; student += "<div class=\"student card bg-light m-2\"
+ * style=\"max-width: 10rem; float: left;\">"; student += "<div
+ * class=\"card-body\">"; student += title + " " + name + ","; student += "<br>";
+ * student += yearNumber + " year"; student += "</div>"; student += "<input
+ * type=\"button\" value=\"Remove\" class=\"btn btn-danger remove\">"; student += "</div>";
+ * 
+ * return student; }
+ */
 
-	switch (year) {
-	case "1":
-		yearNumber = "1st";
-		break;
-	case "2":
-		yearNumber = "2nd";
-		break;
-	case "3":
-		yearNumber = "3rd";
-		break;
-	case "4":
-		yearNumber = "4th";
-		break;
-	}
+// 0000000000000000000000000000000000
+$(document).ready(function() 
+{ 
+	$("#alertSuccess").hide(); 
+	$("#alertError").hide(); 
+}); 
 
-	var student = "";
-	student += "<div class=\"student card bg-light m-2\"       style=\"max-width: 10rem; float: left;\">";
-	student += "<div class=\"card-body\">";
-	student += title + " " + name + ",";
-	student += "<br>";
-	student += yearNumber + " year";
-	student += "</div>";
-	student += "<input type=\"button\" value=\"Remove\"         class=\"btn btn-danger remove\">";
-	student += "</div>";
+// SAVE ============================================
+$(document).on("click", "#btnSave", function(event) 
+{  // Clear status msges-------------
+	$("#alertSuccess").text("");
+	$("#alertSuccess").hide(); 
+	$("#alertError").text(""); 
+	$("#alertError").hide(); 
+	// Form validation----------------  
+	var status = validateItemForm(); 
+	 
+	 // If not valid------------------- 
+	if (status != true)  
+	{  
+		$("#alertError").text(status);
+		$("#alertError").show();  
+		return;  
+	} 
+	 
+	 // If valid---------------------- 
+	var student = getStudentCard($("#txtName").val().trim(),  
+			$('input[name="rdoGender"]:checked').val(),   
+			$("#ddlYear").val());  $("#colStudents").append(student);   
+			$("#alertSuccess").text("Saved successfully.");
+			$("#alertSuccess").show();
+			$("#formStudent")[0].reset();
+}); 
 
-	return student;
-}*/
+//REMOVE========================================== 
+$(document).on("click", ".remove", function(event) 
+{ 
+	$(this).closest(".student").remove();  
+	$("#alertSuccess").text("Removed successfully.");
+	$("#alertSuccess").show();
+}); 
+
+//CLIENT-MODEL================================================================= 
+function validateItemForm()
+{  // NAME 
+	if ($("#txtName").val().trim() == "") 
+	{   
+		return "Insert student name."; 
+	} 
+
+// GENDER  
+	if ($('input[name="rdoGender"]:checked').length === 0) 
+	{ 
+		return "Select gender.";  
+	} 
+
+// YEAR  
+	if ($("#ddlYear").val() == "0") 
+	{   
+		return "Select year.";
+	} 
+
+
+	return true; 
+} 
+
+function getStudentCard(name, gender, year) 
+{  
+	var title = (gender == "Male") ? "Mr." : "Ms."; 
+	var yearNumber = ""; 
+
+	switch (year)
+	{  
+	case "1":   yearNumber = "1st"; 
+			break; 
+	case "2":   yearNumber = "2nd"; 
+			break; 
+	case "3":   yearNumber = "3rd"; 
+			break; 
+	case "4":   yearNumber = "4th"; 
+			break;  
+	} 
+
+var student = ""; 
+student += "<div class=\"student card bg-light m-2\"style=\"max-width: 10rem; float: left;\">";  
+student += "<div class=\"card-body\">";  
+student += title + " " + name + ","; 
+student += "<br>"; 
+student += yearNumber + " year"; 
+student += "</div>";
+student += "<input type=\"button\" value=\"Remove\" class=\"btn btn-danger remove\">"; 
+student += "</div>"; 
+
+return student;
+}
--- helthcare_system/WebContent/visitingDetailsAdding.jsp
+++ helthcare_system/WebContent/visitingDetailsAdding.jsp
@@ -3,23 +3,18 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
-<title>Insert title here</title>
-<link rel="stylesheet" href="Views/bootstrap.min.css"> 
-<script src="Components/jquery-3.2.1.min.js"></script> 
-<script src="Components/main.js"></script>   
+<meta charset="ISO-8859-1">
+<title>Student details</title>
+<link rel="stylesheet" href="Views/bootstrap.min.css">
+<script src="Components/jquery-3.2.1.min.js"></script>
+<script src="Components/main.js"></script>
 </head>
-
-
 <body>
 	<div class="container">
 		<div class="row">
 			<div class="col-8">
-
 				<h1 class="m-3">Student details</h1>
-
 				<form id="formStudent">
-
 					<!-- NAME -->
 					<div class="input-group input-group-sm mb-3">
 						<div class="input-group-prepend">
@@ -27,6 +22,7 @@
 						</div>
 						<input type="text" id="txtName" name="txtName">
 					</div>
+
 					<!-- GENDER -->
 					<div class="input-group input-group-sm mb-3">
 						<div class="input-group-prepend">
@@ -36,6 +32,7 @@
 							name="rdoGender" value="Male"> &nbsp;&nbsp;Female <input
 							type="radio" id="rdoGenderFemale" name="rdoGender" value="Female">
 					</div>
+
 					<!-- YEAR -->
 					<div class="input-group input-group-sm mb-3">
 						<div class="input-group-prepend">
@@ -49,21 +46,20 @@
 							<option value="4">4th year</option>
 						</select>
 					</div>
+
 					<div id="alertSuccess" class="alert alert-success"></div>
 					<div id="alertError" class="alert alert-danger"></div>
+
 					<input type="button" id="btnSave" value="Save"
 						class="btn btn-primary">
 				</form>
 			</div>
 		</div>
-
 		<br>
-
 		<div class="row">
 			<div class="col-12" id="colStudents"></div>
 		</div>
+
 	</div>
 </body>
-
-
 </html>
